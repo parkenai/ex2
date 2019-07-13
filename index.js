476ee7c06e640a020d10fedcc8942fe51bef7164
Pakeenai Khamlaeng <pakeenai169@gmail.com>
2019-07-13 14:13:58 +0700

    เ่มหน้า test

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('Parkenai Khamlaeng'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contact', (req, res) => res.send('ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
